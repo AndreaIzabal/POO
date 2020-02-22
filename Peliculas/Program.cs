1238537bc47f467684b0f112968f23d9774777ff
a <a>
2020-02-21 20:42:49 -0800

    Update Program.cs

--- Peliculas/Program.cs
+++ Peliculas/Program.cs
@@ -2,11 +2,24 @@
 
 namespace Peliculas
 {
+    class Peliculas
+    {
+        public string titulo;
+        public int año;
+        public string pais;
+        public string director;
+
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+         Pelicula p1 = new Pelicula();
+         p1.SetTitulo("La La Land");
+         p1.SetAño(2016); 
+         Console.WriteLine("{0}({1})", p1.GetTitulo(), p1.GetAño());
+ 
         }
     }
 }
