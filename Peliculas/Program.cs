2539e29353c350d4c011e45a968eba7cc34ac585
AndreaIzabal <60375942+AndreaIzabal@users.noreply.github.com>
2020-02-22 09:10:17 -0800

    .

---
+++ Peliculas/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Peliculas
+{
+     class Peliculas
+    {
+        public string Titulo;
+        public int Año;
+        public string pais;
+        public string director;
+        
+        //Constructor
+        public Peliculas()
+        {
+         
+        }
+         public string GetTitulo()
+         {
+             return Titulo;
+         }
+         public string SetTitulo()
+         {
+             return Titulo;
+         }
+         public int GetAño()
+         {
+             return Año;
+         }
+         public int SetAño()
+         {
+             return Año;
+         }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         Peliculas p1 = new Peliculas();
+         p1.SetTitulo("La La Land");
+         p1.SetAño(2016); 
+         Console.WriteLine("{0}({1}");
+         p1.GetTitulo();
+         p1.GetAño();
+         Peliculas p2 = new Peliculas ();
+         p2.SetTitulo("Frozen");
+         p2.SetAño(2016);
+         Console.WriteLine("{0}({2})");
+         p2.GetTitulo();
+         p2.GetAño();
+        }
+    }
+}
