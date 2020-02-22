9f41e708d1dbfa591963f71fd39953603275b680
a <a>
2020-02-21 20:51:06 -0800

    Update Program.cs

--- Peliculas/Program.cs
+++ Peliculas/Program.cs
@@ -5,9 +5,15 @@ namespace Peliculas
     class Peliculas
     {
         public string titulo;
-        public int año;
+        public int16 año;
         public string pais;
         public string director;
+        
+        //Constructor
+        public Pelicula(string T, int16 a )
+        {
+         
+        }
 
 
     }
@@ -19,7 +25,12 @@ namespace Peliculas
          p1.SetTitulo("La La Land");
          p1.SetAño(2016); 
          Console.WriteLine("{0}({1})", p1.GetTitulo(), p1.GetAño());
+         Pelicula p2 = new pelicula();
+         p2.SetTitulo("Frozen");
+         p2.SetAño(2016);
+         Console.WriteLine("{0}({2})"),p2.GetTitulo().p2.GetAño());
  
         }
     }
 }
+
